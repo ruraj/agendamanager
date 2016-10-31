51b2a6814e5adede15a14d167d674ab1ab1e3a81
Ruraj Joshi <ruraj.joshi@rts.com.np>
2016-10-31 14:39:59 -0500

    Check if priority is within range. Started javadoc documentation.

--- src/main/java/com/ruraj/agendamanager/rule/RuleLoader.java
+++ src/main/java/com/ruraj/agendamanager/rule/RuleLoader.java
@@ -15,11 +15,22 @@ public class RuleLoader {
   private final BufferedReader bufferedReader;
   private Pattern rulePattern = Pattern.compile("\\((.*?)\\)");
 
+  /**
+   * Initializes file loader with given file
+   * @param filename Full path of file to load from
+   * @throws FileNotFoundException
+   */
   public RuleLoader(String filename) throws FileNotFoundException {
     File file = new File(filename);
     bufferedReader = new BufferedReader(new FileReader(file));
   }
 
+  /**
+   * Gets the list of rules in next line from file initialized in constructor.
+   * @see RuleLoader
+   * @return ArrayList of rules, null if EOF reached
+   * @throws IOException
+   */
   public List<Rule> nextLine() throws IOException {
     List<Rule> results = new ArrayList<>();
 
@@ -44,9 +55,14 @@ public class RuleLoader {
       Rule rule;
       String[] things = match.substring(1, match.length() - 1).split(",");
 
+      int priority = Integer.valueOf(things[1].trim());
+
+      // Check if priority is between 1 and 100. Otherwise, ignore this rule.
+      if (priority < 1 || priority > 100) continue;
+
       rule = new Rule(
               things[0].trim(),
-              Integer.valueOf(things[1].trim())
+              priority
       );
 
       rules.add(rule);
